18b6c4765e1db6e8cf7023a528b1c0ef2a2ee9c6
Sean Newman <seangnewman@msn.com>
2018-10-03 06:26:15 -0700

    Completed Fundamentals Section

---
+++ newmanuevers.Extensions.Tests/Demo4/IReferenceDataSourceCollectionExtensionTests.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using newmanuevers.Extensions.Samples_v2.Demo3;
+using newmanuevers.Extensions.Samples_v2.Demo3.Impl;
+
+namespace newmanuevers.Extensions.Tests.Demo4
+{
+    [TestClass]
+    public class IReferenceDataSourceCollectionExtensionTests
+    {
+        [TestMethod]
+        public void GetAllItemsByCode_Array()
+        {
+            var sources = new IReferenceDataSource[]
+            {
+                new SqlReferenceDataSource(),
+                new XmlReferenceDataSource(),
+                new ApiReferenceDataSource()
+            };
+
+            var items = sources.GetAllItemsByCode("xyz");
+
+            Assert.AreEqual(6, items.Count());
+        }
+
+        [TestMethod]
+        public void GetAllItemsByCode_ArrayList()
+        {
+            var sources = new ArrayList()
+            {
+                new SqlReferenceDataSource(),
+                new XmlReferenceDataSource(),
+                new ApiReferenceDataSource()
+            };
+
+            sources.Add("I am not a reference data source");
+            var items = sources.GetAllItemsByCode("xyz");
+
+            Assert.AreEqual(6, items.Count());
+        }
+
+        [TestMethod]
+        public void GetAllItemsByCode_IEnumerable()
+        {
+            var sources = new HashSet<IReferenceDataSource>
+            {
+                new SqlReferenceDataSource(),
+                new XmlReferenceDataSource(),
+                new ApiReferenceDataSource()
+            };
+
+            
+            var items = sources.GetAllItemsByCode("xyz");
+
+            Assert.AreEqual(6, items.Count());
+        }
+    }
+}
---
+++ newmanuevers.Extensions.Tests/Demo5/ObjectExtensionsTest.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using newmanuevers.Extensions.Samples_v2.Demo3;
+using newmanuevers.Extensions.Samples_v2.Demo3.Impl;
+using newmanuevers.Extensions.Samples_v2.Demo5;
+
+namespace newmanuevers.Extensions.Tests.Demo5
+{
+    [TestClass]
+    public class ObjectExtensionsTest
+    {
+        [TestMethod]
+        public void ToJsonStringTests()
+        {
+            var obj1 = int.MaxValue;
+            Debug.WriteLine("obj1 - " + obj1.ToJsonString());
+
+            var obj2 = new DateTime(2000, 12, 31);
+            Debug.WriteLine("obj2 - " + obj2.ToJsonString());
+
+            var obj3 = new ReferenceDataItem
+            {
+                Code = "xyz",
+                Description = "123"
+            };
+            Debug.WriteLine("obj3 - " + obj3.ToJsonString());
+
+            IEnumerable<IReferenceDataSource> obj4 = new List<IReferenceDataSource>
+            {
+                new SqlReferenceDataSource()
+            };
+            Debug.WriteLine("obj4 - " + obj4.ToJsonString());
+
+
+        }
+
+        [TestMethod]
+        public void GetJsonTypeDescriptionTests()
+        {
+            var obj1 = int.MaxValue;
+            Debug.WriteLine("obj1 - " + obj1.GetJsonTypeDescription());
+
+            var obj2 = new DateTime(2000, 12, 31);
+            Debug.WriteLine("obj2 - " + obj2.GetJsonTypeDescription());
+
+            var obj3 = new ReferenceDataItem
+            {
+                Code = "xyz",
+                Description = "123"
+            };
+            Debug.WriteLine("obj3 - " + obj3.GetJsonTypeDescription());
+
+            IEnumerable<IReferenceDataSource> obj4 = new List<IReferenceDataSource>
+            {
+                new SqlReferenceDataSource()
+            };
+            Debug.WriteLine("obj4 - " + obj4.GetJsonTypeDescription());
+
+
+        }
+    }
+}
